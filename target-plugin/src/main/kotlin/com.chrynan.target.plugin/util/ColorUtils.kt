84af61c6454aa8e7bd03deccf210c7458f070d4d
Chris <ckeenan@starry.com>
2020-09-15 10:26:01 -0400

    Updated ColorHandler

--- target-plugin/src/main/kotlin/com.chrynan.target.plugin/util/ColorUtils.kt
+++ target-plugin/src/main/kotlin/com.chrynan.target.plugin/util/ColorUtils.kt
@@ -1,18 +1,43 @@
 package com.chrynan.target.plugin.util
 
 import com.chrynan.target.plugin.KotlinTargetColor
+import kotlin.random.Random
 
 class ColorHandler(private val colors: List<KotlinTargetColor>) {
 
-    fun getBackgroundColor(name: String): String {
-        val color = colors.firstOrNull { it.name == name }
+    private val defaultColors = listOf(
+        "#3AD4FF",
+        "#6BAC25",
+        "#FF9B00",
+        "#4B4BFF"
+    )
 
-        return color?.backgroundColor ?: ""
+    private val backgroundColorMap = mutableMapOf<String, String>()
+    private val textColorMap = mutableMapOf<String, String>()
+
+    fun getBackgroundColor(name: String): String {
+        val kotlinTargetColor = colors.firstOrNull { it.name == name }
+
+        val color = kotlinTargetColor?.backgroundColor ?: backgroundColorMap[name] ?: getRandomColor()
+
+        backgroundColorMap[name] = color
+
+        return color
     }
 
     fun getTextColor(name: String): String {
-        val color = colors.firstOrNull { it.name == name }
+        val kotlinTargetColor = colors.firstOrNull { it.name == name }
 
-        return color?.textColor ?: ""
+        val color = kotlinTargetColor?.textColor ?: textColorMap[name] ?: getRandomColor()
+
+        textColorMap[name] = color
+
+        return color
+    }
+
+    private fun getRandomColor(): String {
+        val index = Random.nextInt(defaultColors.size)
+
+        return defaultColors[index]
     }
 }
